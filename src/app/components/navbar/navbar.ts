27723ab0c0556ff3785120b4b7c65e34439dceba
Mikhael Candra <tjandraraharja99@gmail.com>
2026-04-24 11:45:37 +0700

    feat: dynamic style

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar {
+  isScrolled = false
+
+  @HostListener('window:scroll', [])
+  onScroll() {
+    this.isScrolled = window.scrollY > 50
+  }
+}
